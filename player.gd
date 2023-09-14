6ed6a162a1d8469c96e963363b0141424557f027
Dakota Talley <jdtalley16@gmail.com>
2023-09-14 16:20:27 -0400

    Function typing

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ signal hit
 
 var speed = Vector2.ZERO
 
-var points = PackedVector2Array([
+var bounds = PackedVector2Array([
 		Vector2(0.0, -9.0), 
 		Vector2(5.0, 9.0), 
 		Vector2(-5.0, 9.0)
@@ -17,18 +17,18 @@ var points = PackedVector2Array([
 func _ready():
 	pass
 	
-func start(pos):
+func start(pos : Vector2):
 	position = pos
 	show()
-	$CollisionPolygon2D.polygon = points
+	$CollisionPolygon2D.polygon = bounds
 	$CollisionPolygon2D.disabled = false
 
 func _draw():
 	var colors = PackedColorArray([Color.WHITE])
 	
-	draw_polygon(points, colors)
+	draw_polygon(bounds, colors)
 
-func _process(delta):
+func _process(delta : float):
 	var direction = 0
 	if Input.is_action_pressed("rotate_clockwise"):
 		direction = 1
@@ -52,7 +52,7 @@ func _process(delta):
 	position += speed * delta
 
 
-func _on_body_entered(body):
+func _on_body_entered(body : RigidBody2D):
 	hide()
 	hit.emit()
 	$CollisionPolygon2D.set_deferred("disabled", true)
